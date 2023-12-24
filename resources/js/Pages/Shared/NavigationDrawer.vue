5c48451f359e7fbad7cab5fab27e6e3de9ff131e
Modar Nahhas <modar.nahhas@tatweer.sy>
2023-12-24 17:32:41 +0300

    Make navigation drawer expands on hover

--- resources/js/Pages/Shared/NavigationDrawer.vue
+++ resources/js/Pages/Shared/NavigationDrawer.vue
@@ -19,7 +19,7 @@ const links = [
     {
         title: 'Menu',
         linkName: 'menus_index',
-        icon:  'mdi-clipboard-list-outline'
+        icon: 'mdi-clipboard-list-outline'
     },
     {
         title: 'Discounts',
@@ -30,7 +30,10 @@ const links = [
 </script>
 
 <template>
-    <v-navigation-drawer :permanent="true">
+    <v-navigation-drawer
+        :expand-on-hover="true"
+        :rail="true"
+    >
         <v-list>
             <template v-for="link in links">
                 <v-list-item link :title="link.title"
